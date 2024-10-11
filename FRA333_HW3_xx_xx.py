4df95580a81aa1f2f7a288a161e945c7c50708a4
KarD <babykard@gmail.com>
2024-10-11 16:25:57 +0700

    No.1 & 2 Finish
    Jacobian ref frame 0 already

--- FRA333_HW3_xx_xx.py
+++ FRA333_HW3_xx_xx.py
@@ -22,33 +22,34 @@ q = [0,0,0]
 def endEffectorJacobianHW3(q:list[float])->list[float]: #รับค่า q เข้ามา
     R,P,R_e,p_e = FK.FKHW3(q)
 
-    num_joints = len(q) #จำนวนข้อต่อ
-    J_e = np.zeros((6, num_joints)) #สร้าง matrix jacobian 6*3 ตามกำหนด
+    num_joints = len(q) #จำนวนข้อต่อ 3
+    J_0 = np.zeros((6, num_joints)) #สร้าง matrix jacobian 6*3 ตามกำหนด (สำหรับ J3)
+    # R0_i = [[0,0,1],[-np.sin(q[0]),np.cos(q[0]),0],[-np.sin(q[0]),np.cos(q[0]),0]] #กำหนดค่าแกน Zi เทียบ Z0
     z_i = [[0,0,1],[-np.sin(q[0]),np.cos(q[0]),0],[-np.sin(q[0]),np.cos(q[0]),0]] #กำหนดค่าแกน Zi เทียบ Z0
     for i in range(num_joints): #i -> [0,1,2]
 
         P_i = P[:,i] #P01 P02 P03
 
-        Jv_i = np.cross(z_i[i], (p_e - P_i)) # Z1 x (P0e-P01) / Z2 x (P0e-P02) / Z3 x (P0e-P03)
+        Jv_i = np.cross(z_i[i] , (p_e - P_i)) # R0_1*[0,0,1] x (P0e-P01) / R0_2*[0,0,1] x (P0e-P02) / R0_3*[0,0,1] x (P0e-P03)
         # คำนวณ Angular Velocity Jacobian
         Jw_i = z_i[i]
         
         # เพิ่มเข้าไปใน Jacobian
-        J_e[:3, i] = Jv_i  # Linear part
-        J_e[3:, i] = Jw_i  # Angular part
-    #J_e[J_e < 0.000001] = 0
-    return J_e
+        J_0[:3, i] = Jv_i  # Linear part
+        J_0[3:, i] = Jw_i   # Angular part 
+    J_0[abs(J_0) < 0.0001] = 0
+    return J_0
 
 
 #==============================================================================================================#
 #=============================================<คำตอบข้อ 2>======================================================#
 #code here
 def checkSingularityHW3(q:list[float])->bool:
-    J_e = (endEffectorJacobianHW3(q))[:3,:]
+    J_e = (endEffectorJacobianHW3(q))[:3,:3]
     e_val = 0.001
     if abs(np.linalg.det(J_e)) < e_val:
-        flag = 0
-    else:flag = 1
+        flag = 1 #closed singularity
+    else:flag = 0 #normal
     return flag
 #==============================================================================================================#
 #=============================================<คำตอบข้อ 3>======================================================#
@@ -57,11 +58,5 @@ def computeEffortHW3(q:list[float], w:list[float])->list[float]:
     pass
 #==============================================================================================================#
 
-print(endEffectorJacobianHW3(q)) #not correct but why?
-# print(FK.FKHW3(q)[1])
-# print(FK.FKHW3(q)[1][:,1]))
-# print(FK.FKHW3(q)[1][:,2])
-# print(FK.FKHW3(q)[1][:,3])
-#print(endEffectorJacobianHW3(q))
-#print(checkSingularityHW3(q))
-#print(np.linalg.det(endEffectorJacobianHW3(q)[:3,:3]))
+print(endEffectorJacobianHW3(q))
+print(checkSingularityHW3(q))
--- testScript.py
+++ testScript.py
@@ -1,12 +1,11 @@
 # file สำหรับตรวจคำตอบ
 # ในกรณีที่มีการสร้าง function อื่น ๆ ให้ระบุว่า input-output คืออะไรด้วย
 '''
-ชื่อ_รหัส(ex: ธนวัฒน์_6461)
-1.
-2.
+ชื่อ_รหัส(ธนวัฒน์_6461)
+1.นายคณพล__6505
+2.นายอาคม_6577
 3.
 '''
-
 import HW3_utils as FK
 import roboticstoolbox as rtb
 import numpy as np
@@ -16,7 +15,7 @@ from spatialmath.base import tr2rpy
 from spatialmath.base import *
 from math import pi
 
-q = [0,-pi/2,0]
+q = [0,0,0]
 #===========================================<ตรวจคำตอบข้อ 1>====================================================#
 #code here
 R,P,R_e,p_e = FK.FKHW3(q)
@@ -28,7 +27,7 @@ robot = rtb.DHRobot(
     ],tool = SE3(-(0.39243+0.082),-0.093,0.109) * SE3.RPY(0,-pi/2,0,order='zyx')
 )
 #robot.plot(q,block=True)
-sol = robot.jacobe(q)
+sol = robot.jacob0(q)
 sol[abs(sol) < 0.000001] = 0
 print("Jacobain at e = ")
 print(sol)
@@ -36,13 +35,13 @@ print(sol)
 #==============================================================================================================#
 #===========================================<ตรวจคำตอบข้อ 2>====================================================#
 #code here
-J_e = robot.jacobe(q)[:3,:3]
+sol = robot.jacobe(q)[:3,:3]
 e_val = 0.001
-if abs(np.linalg.det(J_e)) < e_val:
-    flag = 1
-else:flag = 0
-print(flag)
-print(abs(np.linalg.det(J_e)))
+if abs(np.linalg.det(sol)) < e_val:
+    flag = 1 #closed singularity
+else:flag = 0 #normal
+print("flag : ",flag)
+
 #==============================================================================================================#
 #===========================================<ตรวจคำตอบข้อ 3>====================================================#
 #code here
